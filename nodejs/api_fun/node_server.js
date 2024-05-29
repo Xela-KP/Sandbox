4adfb14d66afd26b9b06901b60be8244f88c26b8
Alexander King Perocho <perocho.xela@gmail.com>
2024-05-29 18:37:46 -0400

    routed api, home, about on with http module

--- nodejs/api_fun/node_server.js
+++ nodejs/api_fun/node_server.js
@@ -1,8 +1,23 @@
 const http = require("node:http");
 const fs = require("node:fs");
+const { clear } = require("node:console");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, "text/html");
-  fs.createReadStream(__dirname + "/index.html").pipe(res);
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      fs.createReadStream(__dirname + "/index.html").pipe(res);
+      break;
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      fs.createReadStream(__dirname + "/about.html").pipe(res);
+      break;
+    case "/api":
+      res.writeHead(200, { "Content-Type": "application/api" });
+      res.end(JSON.stringify(require("./data.json")));
+      break;
+    default:
+      break;
+  }
 });
 server.listen(3000);
